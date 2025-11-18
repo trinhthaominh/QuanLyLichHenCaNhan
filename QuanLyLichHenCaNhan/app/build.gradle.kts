35731f54e85f01d8eb247ba7751fd08cc2ca5c73
trinhthaominh <92960404+trinhthaominh@users.noreply.github.com>
2025-11-18 20:21:58 +0700

    interface

--- QuanLyLichHenCaNhan/app/build.gradle.kts
+++ QuanLyLichHenCaNhan/app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.kapt")
+    id("com.google.devtools.ksp")
+
 }
 
 android {
@@ -33,6 +36,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -42,7 +48,47 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    val room_version = "2.8.3"
+
+    implementation("androidx.room:room-runtime:$room_version")
+
+// If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
+// See Add the KSP plugin to your project
+    ksp("androidx.room:room-compiler:$room_version")
+
+// If this project only uses Java source, use the Java annotationProcessor
+// No additional plugins are necessary
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+// optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+// optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+// optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+// optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+// optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+// optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
+
+// thêm cho mô hình MVVM & Room bổ sung
+// add bellow dependencies
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.4")
+    implementation ("androidx.recyclerview:recyclerview:1.4.0")
+    implementation ("com.google.android.material:material:1.13.0")
+
+
 }
